6d28c388c50afdeefbc8603102ce82500b98d8ec
mbekri200 <merybekri5@gmail.com>
2023-04-25 20:09:45 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,7 @@
-$rg = 'arm-intriduction-01'
+$rg = 'arm-introduction-01'
 New-AzResourceGroup -Name $rg -Location northeurope -Force
 
 New-AzResourceGroupDeployment `
     -Name 'new-storage' `
     -ResourceGroupName $rg `
-    -TemplateFile 'storagetemp.json' `
+    -TemplateFile 'storagetemp.json' 
